69655124efb2b24130326669ecd24ef92594d498
Luís Antônio <luisan793@gmail.com>
2025-04-16 11:51:02 -0300

    Update

--- src/B1142.java
+++ src/B1142.java
@@ -4,14 +4,13 @@ public class B1142 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        int N = entrada.nextInt();
-        int soma = 1;
+        int n = entrada.nextInt();
+        int numero = 1;
 
-        for (int i = 1; i < N ; i++) {
-            soma = soma + 4;
-            System.out.println(soma);
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d %d %d PUM\n", numero, numero + 1, numero + 2);
+            numero += 4;
         }
-
 
         entrada.close();
     }
--- src/B1145.java
+++ src/B1145.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class B1145 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
+        int x = entrada.nextInt();
+        int y = entrada.nextInt();
+
+        for (int i = 1; i <= y; i++) {
+            if (i % x == 0) {
+                System.out.println(i);
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+
+        entrada.close();
     }
 }
